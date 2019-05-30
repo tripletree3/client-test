4be313417b6967c172120bf3a304b735be80b72e
lizeen <zeen.li@capitalonline.net>
2019-05-30 11:19:31 +0800

    update

--- deployment.go
+++ deployment.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"k8s.io/apimachinery/pkg/api/errors"
+	//"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -31,51 +31,14 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	//for {
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	// Examples for error handling:
-	// - Use helper functions like e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
 	namespace := "default"
-	pod := "myapp-pod1"
-	_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-			pod, namespace, statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		//fmt.Printf("%v\n", cPod)
-	}
-
-	nList, err := clientset.CoreV1().Nodes().List(metav1.ListOptions{})
-	fmt.Printf("Nodes %v\n", len(nList.Items))
-	for i, item := range nList.Items {
-		fmt.Printf("%v: %v %v\n", i, item.Name, item.Status.Capacity)
-	}
-
-	sList, err := clientset.CoreV1().Services(namespace).List(metav1.ListOptions{})
-	fmt.Printf("Services %v\n", len(sList.Items))
-	for i, item := range sList.Items {
-		fmt.Printf("%v: %v %v\n", i, item.Name, item.Spec.ClusterIP)
-	}
 
 	dList, err := clientset.AppsV1().Deployments(namespace).List(metav1.ListOptions{})
 	fmt.Printf("Deployments %v\n", len(dList.Items))
 	for i, item := range dList.Items {
 		fmt.Printf("%v: %v %v %v\n", i, item.Name, item.Status.Replicas, *item.Spec.Replicas)
 	}
-
-	//	time.Sleep(10 * time.Second)
-	//}
 }
 
 func homeDir() string {

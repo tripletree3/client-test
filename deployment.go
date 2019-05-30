df735494abed9ee4d40e2f4c866c20969c0fc304
lizeen <zeen.li@capitalonline.net>
2019-05-30 18:24:43 +0800

    update

--- deployment.go
+++ deployment.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"k8s.io/apimachinery/pkg/api/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"os"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"os"
 	"path/filepath"
 )
 
@@ -20,25 +22,44 @@ func main() {
 	}
 	flag.Parse()
 
-	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	namespace := "default"
+	listDeployments(namespace, clientset)
+	createDeployment(namespace, clientset)
+}
 
-	dList, err := clientset.AppsV1().Deployments(namespace).List(metav1.ListOptions{})
+func listDeployments(ns string, cs *kubernetes.Clientset) {
+	dList, err := cs.AppsV1().Deployments(ns).List(metav1.ListOptions{})
+	if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error get deployment list in namespace %s: %v", ns, statusError)
+		return
+	}
 	fmt.Printf("Deployments %v\n", len(dList.Items))
 	for i, item := range dList.Items {
-		fmt.Printf("%v: %v %v %v\n", i, item.Name, item.Status.Replicas, *item.Spec.Replicas)
+		fmt.Printf("%v: %v %v %v\n", i, item.Name, item.Status.Replicas, item.APIVersion)
 	}
+}
+
+func createDeployment(ns string, cs *kubernetes.Clientset) {
+	var replicas int32 = 2
+	var deployment = appsv1.Deployment{
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"service": "test"},
+			},
+		},
+	}
+	fmt.Printf("%v", deployment)
 }
 
 func homeDir() string {
